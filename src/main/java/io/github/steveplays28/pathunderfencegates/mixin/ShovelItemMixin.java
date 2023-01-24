970ed94cacb9a8567df84a01e9455be9112917c9
Darion Spaargaren <djspaargaren@outlook.com>
2023-01-24 16:26:17 +0100

    refactor: Rewrite using an `@Inject`
    No longer uses an `@Overwrite`, so mod compatibility should be much better.
    Also lowered the mixin's priority to `2000`, so any other mod modifying `ShovelItem` should get applied first.
    Also added `website` metadata to point to Modrinth.
    
    Fixes #4.

--- src/main/java/io/github/steveplays28/pathunderfencegates/mixin/ShovelItemMixin.java
+++ src/main/java/io/github/steveplays28/pathunderfencegates/mixin/ShovelItemMixin.java
@@ -13,58 +13,65 @@ import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldEvents;
+import net.minecraft.world.event.GameEvent;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.util.Map;
 
-@Mixin(ShovelItem.class)
+@Mixin(value = ShovelItem.class, priority = 2000)
 public class ShovelItemMixin {
-    @Final
-    @Shadow
-    protected static Map<Block, BlockState> PATH_STATES;
+	@Final
+	@Shadow
+	protected static Map<Block, BlockState> PATH_STATES;
 
-    /**
-     * @author Steveplays28
-     * @reason I have to modify an if statement in this function so overwriting seems like a better option than injecting
-     */
-    @Overwrite
-    public ActionResult useOnBlock(ItemUsageContext context) {
-        World world = context.getWorld();
-        BlockPos blockPos = context.getBlockPos();
-        BlockState blockState = world.getBlockState(blockPos);
-        if (context.getSide() == Direction.DOWN) {
-            return ActionResult.PASS;
-        } else {
-            PlayerEntity playerEntity = context.getPlayer();
-            BlockState blockState2 = PATH_STATES.get(blockState.getBlock());
-            BlockState blockState3 = null;
-            if (blockState2 != null && (world.getBlockState(blockPos.up()).isAir() || world.getBlockState(blockPos.up()).isIn(BlockTags.FENCE_GATES))) {
-                world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0F, 1.0F);
-                blockState3 = blockState2;
-            } else if (blockState.getBlock() instanceof CampfireBlock && blockState.get(CampfireBlock.LIT)) {
-                if (!world.isClient()) {
-                    world.syncWorldEvent(null, 1009, blockPos, 0);
-                }
+	@Inject(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemUsageContext;getWorld()Lnet/minecraft/world/World;"), locals = LocalCapture.CAPTURE_FAILSOFT, cancellable = true)
+	public void modifyUseOnBlock(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+		World world = context.getWorld();
+		BlockPos blockPos = context.getBlockPos();
+		BlockState blockState = world.getBlockState(blockPos);
 
-                CampfireBlock.extinguish(context.getPlayer(), world, blockPos, blockState);
-                blockState3 = blockState.with(CampfireBlock.LIT, false);
-            }
+		if (context.getSide() != Direction.DOWN) {
+			PlayerEntity playerEntity = context.getPlayer();
+			BlockState blockState2 = PATH_STATES.get(blockState.getBlock());
+			BlockState blockState3 = null;
 
-            if (blockState3 != null) {
-                if (!world.isClient) {
-                    world.setBlockState(blockPos, blockState3, 11);
-                    if (playerEntity != null) {
-                        context.getStack().damage(1, playerEntity, (p) -> p.sendToolBreakStatus(context.getHand()));
-                    }
-                }
+			if (blockState2 != null && (world.getBlockState(blockPos.up()).isAir() || world.getBlockState(blockPos.up()).isIn(BlockTags.FENCE_GATES))) {
+				world.playSound(playerEntity, blockPos, SoundEvents.ITEM_SHOVEL_FLATTEN, SoundCategory.BLOCKS, 1.0f, 1.0f);
+				blockState3 = blockState2;
 
-                return ActionResult.success(world.isClient);
-            } else {
-                return ActionResult.PASS;
-            }
-        }
-    }
+				if (playerEntity != null) {
+					playerEntity.swingHand(context.getHand());
+				}
+			} else if (blockState.getBlock() instanceof CampfireBlock && blockState.get(CampfireBlock.LIT)) {
+				if (!world.isClient()) {
+					world.syncWorldEvent(null, WorldEvents.FIRE_EXTINGUISHED, blockPos, 0);
+				}
+				CampfireBlock.extinguish(context.getPlayer(), world, blockPos, blockState);
+				blockState3 = blockState.with(CampfireBlock.LIT, false);
+
+				if (playerEntity != null) {
+					playerEntity.swingHand(context.getHand());
+				}
+			}
+
+			if (blockState3 != null) {
+				if (!world.isClient) {
+					world.setBlockState(blockPos, blockState3, Block.NOTIFY_ALL | Block.REDRAW_ON_MAIN_THREAD);
+					world.emitGameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Emitter.of(playerEntity, blockState3));
+					if (playerEntity != null) {
+						context.getStack().damage(1, playerEntity, p -> p.sendToolBreakStatus(context.getHand()));
+						playerEntity.swingHand(context.getHand());
+					}
+				}
+				cir.setReturnValue(ActionResult.success(world.isClient));
+			}
+		}
+	}
 }
