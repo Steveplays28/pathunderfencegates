a9dc5222d30593be2c3a73f554f121d663665c8b
Steveplays28 <djspaargaren@outlook.com>
2023-10-07 23:54:26 +0200

    chore: Update to `1.20.1`

--- src/main/java/io/github/steveplays28/pathunderfencegates/util/BlockStateUtil.java
+++ src/main/java/io/github/steveplays28/pathunderfencegates/util/BlockStateUtil.java
@@ -2,14 +2,17 @@ package io.github.steveplays28.pathunderfencegates.util;
 
 import net.minecraft.block.*;
 import net.minecraft.block.enums.SlabType;
-import net.minecraft.tag.BlockTags;
+import net.minecraft.registry.tag.BlockTags;
 
 import static net.minecraft.block.SlabBlock.TYPE;
 
 public class BlockStateUtil {
+	@SuppressWarnings("deprecation")
 	public static boolean isBlockAllowedAboveDirtPathBlock(BlockState blockState) {
 		Block block = blockState.getBlock();
 
-		return !blockState.getMaterial().isSolid() || blockState.isAir() || blockState.isIn(BlockTags.FENCE_GATES) || blockState.isIn(BlockTags.TRAPDOORS) || block instanceof BellBlock || block instanceof WallSignBlock || block instanceof PaneBlock || (block instanceof SlabBlock && blockState.get(TYPE).equals(SlabType.TOP));
+		return !blockState.isSolid() || blockState.isAir() || blockState.isIn(BlockTags.FENCE_GATES) || blockState.isIn(
+				BlockTags.TRAPDOORS) || block instanceof BellBlock || block instanceof WallSignBlock || block instanceof PaneBlock || (block instanceof SlabBlock && blockState.get(
+				TYPE).equals(SlabType.TOP));
 	}
 }
