4c182ef13fc9f08b1ea0f43cc277d7ed6c255eb1
Steveplays28 <djspaargaren@outlook.com>
2023-10-07 23:35:53 +0200

    feat: Implement Manifold CLI argument

--- src/main/java/io/github/steveplays28/pathunderfencegates/util/BlockStateUtil.java
+++ src/main/java/io/github/steveplays28/pathunderfencegates/util/BlockStateUtil.java
@@ -4,7 +4,7 @@ import net.minecraft.block.*;
 import net.minecraft.block.enums.SlabType;
 import org.jetbrains.annotations.NotNull;
 
-#if MC_1_19
+#if MC_1_19_2
 import net.minecraft.tag.BlockTags;
 #else
 import net.minecraft.registry.tag.BlockTags;
@@ -13,11 +13,11 @@ import net.minecraft.registry.tag.BlockTags;
 import static net.minecraft.block.SlabBlock.TYPE;
 
 public class BlockStateUtil {
-	@SuppressWarnings("deprecation")
+	#if MC_1_20_1 @SuppressWarnings("deprecation") #endif
 	public static boolean BlockAllowedAboveDirtPathBlock(@NotNull BlockState blockState) {
 		Block block = blockState.getBlock();
 
-		return #if MC_1_19 !blockState.getMaterial().isSolid() #else !blockState.isSolid() #endif || blockState.isAir() || blockState.isIn(
+		return #if MC_1_19_2 !blockState.getMaterial().isSolid() #else !blockState.isSolid() #endif || blockState.isAir() || blockState.isIn(
 				BlockTags.FENCE_GATES) || blockState.isIn(
 				BlockTags.TRAPDOORS) || block instanceof BellBlock || block instanceof WallSignBlock || block instanceof PaneBlock || (block instanceof SlabBlock && blockState.get(
 				TYPE).equals(SlabType.TOP));
